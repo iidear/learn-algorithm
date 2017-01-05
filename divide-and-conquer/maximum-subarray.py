6b6740e364b68895ad6ffbb303847c2cd6c44467
iidear <iidear2015@gmail.com>
2017-01-05 18:37:23 +0800

    最大子数组问题-分治法

--- divide-and-conquer/maximum-subarray.py
+++ divide-and-conquer/maximum-subarray.py
@@ -5,7 +5,6 @@ A = [13, -3, -25, 20, -3, -16, -23, 18,\
     20, -7, 12, -5, -22, 15, -4, 7]
 
 # 暴力求解
-
 def violence(list):
 	max_subarr_sum = list[0]
 	subarr_start = 0
@@ -20,16 +19,63 @@ def violence(list):
 				subarr_start = i
 				subarr_end = j
 
-	print('subarr_start:', subarr_start)
-	print('subarr_end:', subarr_end)
-	print('max_subarr_sum:', max_subarr_sum)
+	return (subarr_start, subarr_end, max_subarr_sum)
 
-	print('maximum subarray: [', end='')
-	for i in range(subarr_start, subarr_end):
-		print(list[i], ',', end='')
-	print(list[subarr_end],']', sep='')
+print('暴力求解结果：')
+(subarr_start, subarr_end, max_subarr_sum) = violence(A)
+print('subarr_start:', subarr_start)
+print('subarr_end:', subarr_end)
+print('max_subarr_sum:', max_subarr_sum)
 
 
-violence(A)
+# 分治法
+## 求解跨越中间元素的最大子数组
+def divideCross(A, low, mid, high):
+	left_sum = float('-inf')
+	sum = 0
+	for i in range(mid, low-1, -1):
+		sum += A[i]
+		if sum > left_sum:
+			left_sum = sum
+			max_left = i
 
-#分治法
+	right_sum = float('-inf')
+	sum = 0
+	for j in range(mid+1, high+1):
+		sum += A[j]
+		if sum > right_sum:
+			right_sum = sum
+			max_right = j
+	return (max_left, max_right, left_sum+right_sum)
+
+
+def divide(A, low, high):
+
+	# base case
+	if high == low:
+		return (low, high, A[low])
+
+	# recursive case
+	else:
+		# divide
+		mid = int((low + high)/2)
+
+		# conquer
+		(left_low, left_high, left_sum) = divide(A, low, mid)
+		(right_low, right_high, right_sum) = divide(A, mid+1, high)
+		(cross_low, cross_high, cross_sum) = divideCross(A, low, mid, high)
+
+		# combine
+		if left_sum >= right_sum and left_sum >= cross_sum:
+			return (left_low, left_high, left_sum)
+		elif right_sum >= left_sum and right_sum >= cross_sum:
+			return (right_low, right_high, right_sum)
+		else:
+			return (cross_low, cross_high, cross_sum)
+	
+
+print('分治法结果：')
+(subarr_start, subarr_end, max_subarr_sum) = divide(A, 0, len(A)-1)
+print('subarr_start:', subarr_start)
+print('subarr_end:', subarr_end)
+print('max_subarr_sum:', max_subarr_sum)
